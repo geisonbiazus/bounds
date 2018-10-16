0b2c2f301be75e8443ff5b7034f7f2ecd21efdd2
Geison Biazus <geisonbiazus@gmail.com>
2018-10-16 20:12:24 -0300

    The beginning

---
+++ lib/bounds.ex
@@ -0,0 +1,18 @@
+defmodule Bounds do
+  @moduledoc """
+  Documentation for Bounds.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bounds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bounds_test.exs
@@ -0,0 +1,8 @@
+defmodule BoundsTest do
+  use ExUnit.Case
+  doctest Bounds
+
+  test "greets the world" do
+    assert Bounds.hello() == :world
+  end
+end
