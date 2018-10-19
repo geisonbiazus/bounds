6abbc308e159a7e23ff0f7139989ed9c42eabeb6
Geison Biazus <geisonbiazus@gmail.com>
2018-10-19 20:29:26 -0300

    Refactor on CoordinateFileReader

--- lib/bounds/coordinate_file_reader.ex
+++ lib/bounds/coordinate_file_reader.ex
@@ -1,10 +1,22 @@
 defmodule Bounds.CoordinateFileReader do
-  def read(stream) do
-    stream
-    |> Stream.drop(1)
-    |> Stream.map(&String.split(&1, ","))
-    |> Stream.map(&to_coordinate(&1))
-    |> Stream.filter(fn x -> !is_nil(x) end)
+  def read(rows) do
+    rows
+    |> skip_header
+    |> split_columns
+    |> convert_into_coordinates
+    |> remove_nil
+  end
+
+  defp skip_header(rows) do
+    Stream.drop(rows, 1)
+  end
+
+  defp split_columns(rows) do
+    Stream.map(rows, &String.split(&1, ","))
+  end
+
+  defp convert_into_coordinates(rows) do
+    Stream.map(rows, &to_coordinate(&1))
   end
 
   defp to_coordinate([lon, lat]) do
@@ -13,7 +25,9 @@ defmodule Bounds.CoordinateFileReader do
     Bounds.Coordinate.new(lon, lat)
   end
 
-  defp to_coordinate(_) do
-    nil
+  defp to_coordinate(_), do: nil
+
+  defp remove_nil(rows) do
+    Stream.filter(rows, fn x -> !is_nil(x) end)
   end
 end
