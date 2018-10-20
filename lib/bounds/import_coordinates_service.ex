dc729111e765f625a60039f7d879748ba0111a1d
Geison Biazus <geisonbiazus@gmail.com>
2018-10-20 17:56:21 -0300

    Task 1 complete: Assign coordinates to bounding boxes

--- lib/bounds/import_coordinates_service.ex
+++ lib/bounds/import_coordinates_service.ex
@@ -1,8 +1,16 @@
 defmodule Bounds.ImportCoordinatesService do
-  def run(pairs_file_path) do
-    pairs_file_path
-    |> File.stream!()
-    |> Bounds.CoordinateFileReader.read()
-    |> Bounds.BoundingBoxBuilder.build_list()
+  def run(pairs_file_path, coordinates_file_path) do
+    bounding_boxes =
+      pairs_file_path
+      |> File.stream!()
+      |> Bounds.CoordinateFileReader.read()
+      |> Bounds.BoundingBoxBuilder.build_list()
+
+    coordinates =
+      coordinates_file_path
+      |> File.stream!()
+      |> Bounds.CoordinateFileReader.read()
+
+    Bounds.CoordinateAssigner.assign(bounding_boxes, coordinates)
   end
 end
--- test/bounds/import_coordinates_service_test.exs
+++ test/bounds/import_coordinates_service_test.exs
@@ -1,13 +1,20 @@
 defmodule Bounds.ImportCoordinatesServiceTest do
   use ExUnit.Case, async: true
 
-  test "imports a pairs file and generates bounding boxes" do
+  test "generates bounding boxes assigning the coordinates from the given files" do
     pairs_path = "#{__DIR__}/files/pairs.csv"
-    list = Bounds.ImportCoordinatesService.run(pairs_path)
+    coordinates_path = "#{__DIR__}/files/coordinates.csv"
+    result = Bounds.ImportCoordinatesService.run(pairs_path, coordinates_path)
 
-    assert list == [
-             Bounds.BoundingBox.new(1.0, 3.0, 9.0, 8.0),
-             Bounds.BoundingBox.new(1.3, 2.3, 2.5, 5.3)
+    assert result == [
+             %{
+               bounding_box: Bounds.BoundingBox.new(1.0, 3.0, 9.0, 8.0),
+               coordinates: [Bounds.Coordinate.new(7.0, 7.0)]
+             },
+             %{
+               bounding_box: Bounds.BoundingBox.new(1.3, 2.3, 2.5, 5.3),
+               coordinates: [Bounds.Coordinate.new(2.1, 2.9)]
+             }
            ]
   end
 end
